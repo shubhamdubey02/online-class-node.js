0d5241f1f9d8886ea9ca4ef5e01d39ae0c3f6da1
Shubham dubey <shubham.dubey@oodles.io>
2025-03-24 18:12:27 +0530

    add product

--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFormFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
-    save() {
-        products.push(this);
+    save(){
+        getProductsFormFile(Product=>{
+            Product.push(this);
+            fs.writeFile(p,JSON.stringify(Product),err=>{
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFormFile(cb);
     }
 };
